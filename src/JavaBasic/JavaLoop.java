5e3e6baa4b199fd7156507d0df77d6ad23d5127c
yajib16 <50978166+yajib16@users.noreply.github.com>
2023-02-09 17:09:23 -0600

    first commit

---
+++ src/JavaBasic/JavaLoop.java
@@ -0,0 +1,29 @@
+package JavaBasic;
+
+public class JavaLoop {
+    static int num=10;
+
+    // this code is used for different loops in java.
+    /*
+    * for loop
+    * while loop
+    * do-while loop
+    * for each loop
+    * */
+    // 1. code for printing number 1 to 20
+//    public static void main(String[] args) {
+//        for (int i = 0; i <= num; i++) {
+//            System.out.println(i);
+//        }
+//        System.out.println("bijay");
+//    }
+
+    // 2. code while loop for same operation
+    public static void main(String[] args) {
+        int num=10;
+        int num2=12;
+        System.out.println(num>num2);
+
+    }
+
+}
---
+++ src/JavaBasic/TernaryOperator.java
@@ -0,0 +1,11 @@
+package JavaBasic;
+
+public class TernaryOperator {
+    //ternary operator
+    //syntax for it: variable = (expression)? condition1:condition2;
+    public static void main(String[] args) {
+        int time =20;
+        String result= (time<18)?"Good Day":"good night";
+        System.out.println(result);
+    }
+}
